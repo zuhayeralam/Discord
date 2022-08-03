ed08e87b36539a90e71614edb5459174bba83992
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-08-03 11:13:52 +1000

    MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,14 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('database connection failed. Server not started');
+    console.error(err);
+  });
